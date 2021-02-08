84c2dd104df231da417e957886e512213f6eb4c8
JJ Bussert <jj.bussert@gmail.com>
2021-02-08 15:54:59 -0600

    adding batch 1 files

---
+++ src/E13.Common.RestEase/AuthClient.cs
@@ -0,0 +1,29 @@
+﻿//using RestEase;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace E13.Common.ApiClient
+//{
+//    public static class AuthClient
+//    {
+//        //public static TApi For<TApi>(string baseUrl)
+//        //{
+//        //    var context = new AuthenticationContext();
+//        //    RestClient.For<TApi>(baseUrl, async (request, cancellationToken) =>
+//        //    {
+//        //        // See if the request has an authorize header
+//        //        var auth = request.Headers.Authorization;
+//        //        if (auth != null)
+//        //        {
+//        //            // The AquireTokenAsync call will prompt with a UI if necessary
+//        //            // Or otherwise silently use a refresh token to return a valid access token 
+//        //            var token = await context.AcquireTokenAsync("http://my.service.uri/app", "clientId", new Uri("callback://complete")).ConfigureAwait(false);
+//        //            request.Headers.Authorization = new AuthenticationHeaderValue(auth.Scheme, token);
+//        //        }
+//        //    });
+
+//        //    return TApi;
+//        //}
+//    }
+//}
---
+++ src/E13.Common.RestEase/IAuthenticatedApi.cs
@@ -0,0 +1,12 @@
+﻿using RestEase;
+using System;
+using System.Net.Http.Headers;
+
+namespace E13.Common.ApiClient
+{
+    public interface IAuthenticatedApi : IRestEaseApi
+    {
+        [Header("Authorization")]
+        AuthenticationHeaderValue Authorization { get; set; }
+    }
+}
