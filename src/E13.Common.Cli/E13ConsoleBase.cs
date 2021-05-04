33f1ce0bd2c235b2ae6f5e4d894c673784e9d822
JJ Bussert <jj.bussert@gmail.com>
2021-05-04 14:10:28 -0500

    new Common Cli lib for creating custom Command Line tools

---
+++ src/E13.Common.Cli/E13ConsoleBase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E13.Cli
+{
+    /// <summary>
+    /// Base implementation of CliConsole used for E13.Tech CLI commands
+    /// 
+    /// Also serves as a concrete example to use for reference to build your own
+    /// </summary>
+    public abstract class E13ConsoleBase : CliConsole
+    {
+        /// <summary>
+        /// Simple Empty Constructor relaying ILogger to base(...)
+        /// </summary>
+        /// <param name="logger"></param>
+        public E13ConsoleBase(ILogger logger)
+            : base(logger) { }
+
+        /// <summary>
+        /// Simple Non-Interactive CLI header
+        /// </summary>
+        protected override void NonInteractiveHeader()
+        {
+            Console.Write("E13: ");
+        }
+    }
+}
---
+++ src/E13.Common.Cli/E13ConsoleSimple.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E13.Cli
+{
+    /// <summary>
+    /// Default implementation of CliConsole used for E13.Tech CLI commands
+    /// 
+    /// Also serves as a concrete example to use for reference to build your own
+    /// </summary>
+    public class E13ConsoleSimple : E13ConsoleBase
+    {
+        /// <summary>
+        /// Simple Empty Constructor relaying ILogger to base(...)
+        /// </summary>
+        /// <param name="logger"></param>
+        public E13ConsoleSimple(ILogger logger)
+            : base(logger) { }
+
+        /// <summary>
+        /// Interactive CLI header with a single color ASCII version of the E13 Logo
+        /// </summary>
+        protected override void InteractiveHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("                                                                  ");
+            Console.WriteLine("    TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT    ");
+            Console.WriteLine("    TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT    ");
+            Console.WriteLine("    TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT    ");
+            Console.WriteLine("                                                                  ");
+            Console.WriteLine("    EEEEEEEEEEEEEEEEEEEEEE   111111   333333333333333333333333    ");
+            Console.WriteLine("    EEEEEEEEEEEEEEEEEEEEEE   111111   333333333333333333333333    ");
+            Console.WriteLine("    EEEEEEEEEEEEEEEEEEEEEE   111111   333333333333333333333333    ");
+            Console.WriteLine("    EEEEEE                   111111                     333333    ");
+            Console.WriteLine("    EEEEEE                   111111                     333333    ");
+            Console.WriteLine("    EEEEEEEEEEEEEEEEEEEE     111111     3333333333333333333333    ");
+            Console.WriteLine("    EEEEEEEEEEEEEEEEEEEE     111111     3333333333333333333333    ");
+            Console.WriteLine("    EEEEEEEEEEEEEEEEEEEE     111111     3333333333333333333333    ");
+            Console.WriteLine("    EEEEEE                   111111                     333333    ");
+            Console.WriteLine("    EEEEEE                   111111                     333333    ");
+            Console.WriteLine("    EEEEEEEEEEEEEEEEEEEEEE   111111   333333333333333333333333    ");
+            Console.WriteLine("    EEEEEEEEEEEEEEEEEEEEEE   111111   333333333333333333333333    ");
+            Console.WriteLine("    EEEEEEEEEEEEEEEEEEEEEE   111111   333333333333333333333333    ");
+            Console.WriteLine("                                                                  ");
+        }
+    }
+}
