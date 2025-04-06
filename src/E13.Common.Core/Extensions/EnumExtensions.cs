a5fa4b742fb985cd13b55b7ccba40ca55cc90df0
JJ Bussert <jj.bussert@gmail.com>
2025-04-06 12:20:29 -0500

    Refactoring to reuse code between extensions and some add null checking to address warnings

--- src/E13.Common.Core/Extensions/EnumExtensions.cs
+++ src/E13.Common.Core/Extensions/EnumExtensions.cs
@@ -16,48 +16,53 @@ namespace System
         /// </summary>
         /// <param name="value">enum to extend</param>
         /// <returns>DisplayAttribute.StringValue</returns>
-        public static string AsDisplay(this Enum value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
+        /// <exception cref="ArgumentException">Thrown if the enum does not have an Attribute of type DisplayAttribute or if the enum itself was not found</exception>
+        public static string AsDisplay(this Enum value) =>
+            value.AsValue<DisplayAttribute, string>((a) => a.StringValue);
 
-            var type = value.GetType();
-            var name = Enum.GetName(type, value);
-            var attribute = type.GetField(name).GetCustomAttribute<DisplayAttribute>();
-
-            return attribute.StringValue;
-        }
         /// <summary>
         /// Gets the StringValue of the associated AbbrevationAttribute
         /// </summary>
         /// <param name="value">enum to extend</param>
         /// <returns>AbbrevationAttribute.StringValue</returns>
-        public static string AsAbbreviation(this Enum value)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
+        /// <exception cref="ArgumentException">Thrown if the enum does not have an Attribute of type AbbrevationAttribute or if the enum itself was not found</exception>
+        public static string AsAbbreviation(this Enum value) =>
+            value.AsValue<AbbrevationAttribute, string>((a) => a.StringValue);
 
-            var type = value.GetType();
-            var name = Enum.GetName(type, value);
-            var attribute = type.GetField(name).GetCustomAttribute<AbbrevationAttribute>();
-
-            return attribute.StringValue;
-        }
         /// <summary>
         /// Gets the Value of the associated GuidAttribute
         /// </summary>
         /// <param name="value">enum to extend</param>
         /// <returns>GuidAttribute.Value</returns>
-        public static Guid AsGuid(this Enum value)
+        /// <exception cref="ArgumentException">Thrown if the enum does not have an Attribute of type GuidAttribute or if the enum itself was not found</exception>
+        public static Guid AsGuid(this Enum value) => 
+            value.AsValue<GuidAttribute, Guid>((a) => a.Value);
+
+        /// <summary>
+        /// Private method used by other extension methods to get an appropriate value for an enum's attribute
+        /// </summary>
+        /// <typeparam name="TAttributeType">Attribute Type to be resolved to retrieve the value</typeparam>
+        /// <typeparam name="TValue">Type of the value to be returned</typeparam>
+        /// <param name="e">enum value</param>
+        /// <param name="valueFunc">Function to resolve the return value</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">Thrown if the enum does not have an Attribute of TAttributeType or if the enum itself was not found</exception>
+        private static TValue AsValue<TAttributeType, TValue>(this Enum e, Func<TAttributeType, TValue> valueFunc)
+            where TAttributeType : Attribute
         {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
+            ArgumentNullException.ThrowIfNull(e);
 
-            var type = value.GetType();
-            var name = Enum.GetName(type, value);
-            var attribute = type.GetField(name).GetCustomAttribute<GuidAttribute>();
+            var type = e.GetType();
+            var name = Enum.GetName(type, e)
+                ?? throw new ArgumentException($"Enum value '{e}' not found in type '{type.Name}'", nameof(e));
 
-            return attribute.Value;
+            var nameAttr = type.GetField(name)
+                ?? throw new ArgumentException($"Enum {name} not found in type {type.Name}");
+
+            var attribute = nameAttr.GetCustomAttribute<TAttributeType>()
+                ?? throw new ArgumentException($"Enum {name} does not have a {nameof(TAttributeType)}");
+
+            return valueFunc.Invoke(attribute);
         }
     }
 }
